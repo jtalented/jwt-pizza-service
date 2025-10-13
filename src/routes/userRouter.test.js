01285792e3be21553fb28745e26625051cd5bdd6
Jayden Allen <141792520+jtalented@users.noreply.github.com>
2025-10-13 17:31:24 -0600

    implement user listing and deletion endpoints

--- src/routes/userRouter.test.js
+++ src/routes/userRouter.test.js
@@ -146,7 +146,7 @@ test('admin can update anyuser', async () => {
     password: 'adminupdatedpass'
   };
   
-
+  
 
   
   const res = await request(app)
@@ -159,4 +159,70 @@ test('admin can update anyuser', async () => {
   expect(res.body.user.name).toBe('Admin Updated Name');
   expectValidJwt(res.body.token);
 });
+
+
+
+
+
+test('list users unauthorized', async () => {
+  const listUsersRes = await request(app).get('/api/user');
+  expect(listUsersRes.status).toBe(401);
+});
+
+
+
+
+
+
+
+
+test('list users', async () => {
+  const [user, userToken] = await registerUser(request(app));
+  const listUsersRes = await request(app)
+    .get('/api/user')
+    .set('Authorization', 'Bearer ' + userToken);
+  expect(listUsersRes.status).toBe(200);
+  expect(listUsersRes.body.users).toBeDefined();
+  expect(listUsersRes.body.more).toBeDefined();
+});
+
+
+
+
+
+
+test('delete user unauthorized', async () => {
+  const deleteUserRes = await request(app).delete('/api/user/1');
+  expect(deleteUserRes.status).toBe(401);
+});
+
+
+
+
+
+test('delete user', async () => {
+  const [user, userToken] = await registerUser(request(app));
+  const deleteUserRes = await request(app)
+    .delete(`/api/user/${user.id}`)
+    .set('Authorization', 'Bearer ' + adminAuthToken);
+  expect(deleteUserRes.status).toBe(200);
+});
+
+
+
+
+
+
+
+async function registerUser(service) {
+  const testUser = {
+    name: 'pizza diner',
+    email: `${randomName()}@test.com`,
+    password: 'a',
+  };
+  const registerRes = await service.post('/api/auth').send(testUser);
+  registerRes.body.user.password = testUser.password;
+
+  return [registerRes.body.user, registerRes.body.token];
+}
 
